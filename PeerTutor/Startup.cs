507c8d9bf0077fe235797ef332dae0ad39602827
Isaac Mbuotidem <mbuotidemisaac@gmail.com>
2019-07-12 18:43:51 -0500

    Added basic login functionality (authentication and authorization) using ASP.NET Core Identity. Added scaffolded Courses controller and views.

--- PeerTutor/Startup.cs
+++ PeerTutor/Startup.cs
@@ -38,6 +38,7 @@ namespace PeerTutor
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
